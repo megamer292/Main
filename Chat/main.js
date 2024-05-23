84ab9ab694bfcc28edff8c301b701c611928e01d
megamer292 <136795907+megamer292@users.noreply.github.com>
2024-05-23 11:42:48 -0700

    Gun basics added

--- Chat/main.js
+++ Chat/main.js
@@ -1,2 +1,5 @@
-
-alert('yoooooooo');
+var gun = GUN(['http://localhost:8765/gun', 'https://gun-manhattan.herokuapp.com/gun']);
+var paste = document.getElementById('paste')
+var copy = gun.get('test').get('paste');
+paste.oninput = () => { copy.put(paste.value) };
+copy.on((data) => { paste.value = data });
