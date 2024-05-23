9eedc844bbda9557d341234bacd4e4950fd7e9e2
megamer292 <136795907+megamer292@users.noreply.github.com>
2024-05-23 15:19:00 -0700

    Update main.js

--- Chat/main.js
+++ Chat/main.js
@@ -1,5 +1,5 @@
 var gun = GUN(['https://megamer292.github.io/Main/Chat/index.html']);
-var paste = document.getElementById('paste')
+var paste = document.getElementById('paste');
 var copy = gun.get('test').get('paste');
 paste.oninput = () => { copy.put(paste.value) };
 copy.on((data) => { paste.value = data });
