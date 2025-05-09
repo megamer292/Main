06dbe21588e63229c134d8ab206543222389122a
megamer292 <136795907+megamer292@users.noreply.github.com>
2025-05-09 13:50:03 -0700

    Update nodetest.js

--- nodetest.js
+++ nodetest.js
@@ -3,25 +3,40 @@ const socketio = require('socket.io');
 const fs = require('fs');
 const readline = require('readline');
 const { stdin: input, stdout: output } = require('node:process');
+
 const rl = readline.createInterface({ input, output });
+const commands = {
+    "code": (arguments) => {
+        if (arguments[0] === "server") {
+            eval(arguments.join(' '));
+        }
+        else if (arguments[0] === "client") {
+            if(arguments[1] === "all") {
+                arguments = arguments.slice(2);
+                io.emit('code', arguments.join(' '));
+            }
+        }
+    }
+}
 
 rl.on("line", data => {
- io.emit("code", data);
+    const arguments = data.split(" ");
+    commands[arguments.shift()](arguments);
 });
 
 var myText = fs.readFileSync('index.html');
 
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end(myText);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(myText);
 });
 
 io = new socketio.Server(server, {
-  cors: {
-    origin: "*"
-  }
+    cors: {
+        origin: "*"
+    }
 });
 
 io.on('connection', client=>{
