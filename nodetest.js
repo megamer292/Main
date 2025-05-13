6c3cb6a5e09aaa3e10bb72117f0d9a064cbe57c9
megamer292 <136795907+megamer292@users.noreply.github.com>
2025-05-13 11:35:40 -0700

    Update nodetest.js

--- nodetest.js
+++ nodetest.js
@@ -51,8 +51,7 @@ io.on('connection', socket => {
     
     socket.on("message", (message) => {
         if (message.reciever == "all") {
-            console.log("MESSAGE RECIEVED");
-            console.log(message.body+" "+message.reciever);
+            console.log("From "+socket.custom.userID+"to "+message.reciever+": "+message.body);
             io.emit("message", {
                 body: message.body,
                 reciever: message.reciever,
