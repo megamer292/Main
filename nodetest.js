a43973e65bf1a01851164e251fb855266a274820
megamer292 <136795907+megamer292@users.noreply.github.com>
2025-05-16 10:08:00 -0700

    Update nodetest.js

--- nodetest.js
+++ nodetest.js
@@ -62,12 +62,7 @@ io.on('connection', socket => {
     };
 
     if (returningUsers[socket.handshake.address] != undefined) {
-        let returnedUser =  returningUsers[socket.handshake.address];
-        let keys = Object.keys(returningUsers);
-        let values =  Object.values(returningUsers);
-        for (i = 0; i < keys.length; i++) {
-            socket.custom[keys[i]] = values[i];
-        }
+        socket.custom = returningUsers[socket.handshake.address];
     }
     
     socket.on("changeUserID", newID => {
@@ -94,7 +89,7 @@ io.on('connection', socket => {
 
     socket.on("disconnecting", (reason) => {
         console.log(socket.custom.userID+" ("+socket.custom.person+")"+" has disconnected")
-        returningUsers[socket.handshake.address] = socket.custom
+        returningUsers[socket.handshake.address] = socket.custom;
     });
     
     console.log(socket.custom.userID +" ("+socket.custom.person+")" + ' HAS CONNECTED FROM ' + socket.handshake.address);
